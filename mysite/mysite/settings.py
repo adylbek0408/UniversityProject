af48cdcb1a3b00a8df3ed35bb40ed11fdd4f4d0e
adylbek0408 <adylbek0408@icloud.com>
2024-06-09 16:39:31 +0600

    efgdh

--- mysite/mysite/settings.py
+++ mysite/mysite/settings.py
@@ -31,6 +31,8 @@ ALLOWED_HOSTS = []
 
 
 # Application definition
+###
+
 
 INSTALLED_APPS = [
     'allauth',
@@ -49,7 +51,6 @@ INSTALLED_APPS = [
     'rest_framework',
     'django_filters',
 ]
-
 
 
 MIDDLEWARE = [
@@ -117,13 +118,21 @@ AUTH_PASSWORD_VALIDATORS = [
 # Internationalization
 # https://docs.djangoproject.com/en/5.0/topics/i18n/
 
-LANGUAGE_CODE = 'en-us'
+LANGUAGE_CODE = 'ru'
 
-TIME_ZONE = 'UTC'
+USE_TZ = True
+
+USE_L10N = True
 
 USE_I18N = True
 
-USE_TZ = True
+TIME_ZONE = 'UTC'
+
+LANGUAGES = (
+    ('ru', 'Russian'),
+    ('en', 'English'),
+    ('ky', 'kyrgyz'),
+)
 
 
 # Static files (CSS, JavaScript, Images)
